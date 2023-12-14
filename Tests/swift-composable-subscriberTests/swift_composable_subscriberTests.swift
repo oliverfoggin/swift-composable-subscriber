50f602f3b9ce535f81f1da8173f36de92440392b
Oliver Foggin <oliverfoggin@me.com>
2023-12-14 17:44:02 +0000

    Initial Commit

---
+++ Tests/swift-composable-subscriberTests/swift_composable_subscriberTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import swift_composable_subscriber
+
+final class swift_composable_subscriberTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
