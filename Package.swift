3382f48991fa78416b46236900bb8435108cc778
Oliver Foggin <oliverfoggin@me.com>
2023-12-14 17:48:20 +0000

    Initial

--- Package.swift
+++ Package.swift
@@ -4,20 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-composable-subscriber",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "swift-composable-subscriber",
-            targets: ["swift-composable-subscriber"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "swift-composable-subscriber"),
-        .testTarget(
-            name: "swift-composable-subscriberTests",
-            dependencies: ["swift-composable-subscriber"]),
-    ]
+	name: "swift-composable-subscriber",
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v6),
+	],
+	products: [
+		.library(name: "ComposableSubscriber", targets: ["ComposableSubscriber"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/pointfreeco/swift-composable-architecture", .upToNextMajor(from: "1.0.0")),
+	],
+	targets: [
+		.target(
+			name: "ComposableSubscriber",
+			dependencies: [
+				.product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+			]
+		),
+	]
 )
---
+++ Sources/ComposableSubscriber/SubscriberReducer.swift
@@ -0,0 +1,59 @@
+import ComposableArchitecture
+
+extension Reducer {
+	func subscribe<TriggerAction, T>(
+		on triggerAction: CaseKeyPath<Action, TriggerAction>,
+		to stream: @escaping () async throws -> AsyncStream<T>,
+		with responseAction: CaseKeyPath<Action, T>
+	) -> _SubscribeReducer<Self, TriggerAction, T> {
+		.init(
+			parent: self,
+			on: triggerAction,
+			to: stream,
+			with: responseAction
+		)
+	}
+}
+
+struct _SubscribeReducer<Parent: Reducer, TriggerAction, T>: Reducer {
+	@usableFromInline
+	let parent: Parent
+
+	@usableFromInline
+	let triggerAction: AnyCasePath<Parent.Action, TriggerAction>
+
+	@usableFromInline
+	let stream: () async throws -> AsyncStream<T>
+
+	@usableFromInline
+	let responseAction: AnyCasePath<Parent.Action, T>
+
+	init(
+		parent: Parent,
+		on triggerAction: CaseKeyPath<Parent.Action, TriggerAction>,
+		to stream: @escaping () async throws -> AsyncStream<T>,
+		with responseAction: CaseKeyPath<Parent.Action, T>
+	) {
+		self.parent = parent
+		self.triggerAction = AnyCasePath(triggerAction)
+		self.stream = stream
+		self.responseAction = AnyCasePath(responseAction)
+	}
+
+	func reduce(into state: inout Parent.State, action: Parent.Action) -> Effect<Parent.Action> {
+		let effects = parent.reduce(into: &state, action: action)
+
+		guard self.triggerAction.extract(from: action) != nil else {
+			return effects
+		}
+
+		return .merge(
+			effects,
+			.run { send in
+				for await value in try await stream() {
+					await send(responseAction.embed(value))
+				}
+			}
+		)
+	}
+}
